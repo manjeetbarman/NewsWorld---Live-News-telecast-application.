4e0e6252caf046fe2c84a2a8e5adb379d2fa91a1
Manjeet Barman <manjitbarman89@gmail.com>
2024-12-02 21:55:08 +0530

    Bg change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       
-      <div>
+      <div style={{ backgroundColor: '#99ff33' }}>
         <Navbar/>
         <News/>
         <Footer/>
